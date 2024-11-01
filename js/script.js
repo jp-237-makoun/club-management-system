a3b737afbfc5c10a3cf60979aa638002a15e29ff
jeanphilippe <jeanphilippe237p@gmail.com>
2024-11-01 01:02:58 +0100

    my club management system

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link')
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=>{
+    wrapper.classList.add(active);
+});
+
+loginLink.addEventListener('click', ()=>{
+    wrapper.classList.remove(active);
+});
+
+btn.addEventListener('click', ()=>{
+    wrapper.classList.add(active);
+});
